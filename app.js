e3a95121c09d883ac9f0616a67fd8870ad2baa0e
Mike Hockerman <mike@hockerman.com>
2016-11-02 18:47:01 -0400

    Help text

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+const LOCKS = {
+  "bwo": "3768",
+  "bwi": "3766",
+  "bs": "3767",
+  "do": "3641",
+  "di": "3640",
+}
+
+const printHelp = () => {
+  return "```\n"
+    + "Control access to the SpeakEasy.\n"
+    + "  /speakeasy help          :: display this text\n"
+    + "  /speakeasy unlock {door} :: unlock a specific door\n"
+    + "Doors:\n"
+    + "  - bwo :: broad ripple / west side / outer\n"
+    + "  - bwi :: broad ripple / west side / inner\n"
+    + "  - bs  :: broad ripple / south side (parking lot)\n"
+    + "  - do  :: downtown / outer\n"
+    + "  - di  :: downtown / inner\n" 
+    + "```"
+}
+
 const bunyan = require('bunyan')
 const log = bunyan.createLogger({ name: 'tfo_slack' })
 
@@ -10,10 +32,17 @@ app.use(bp.urlencoded({
   extended: true
 }))
 
+const kisi = require('kisi-client')
+const k = new kisi.default()
+
 app.post('/speakeasy', (req, res) => {
-  log.info(req.body)
-  log.info(req.query)
-  res.status(200).send("Great!")
+  const { text } = req.body
+  const split = text.split(" ")
+  switch (split) {
+    case 'help':
+      return res.send(printHelp())
+  }
+  res.send("Great!")
 })
 
 app.listen(process.env.PORT, () => {
