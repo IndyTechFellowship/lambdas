50f3d95d7eb79ca1ed4eaefd930e45484e2527ed
Mike Hockerman <mike@hockerman.com>
2016-11-02 18:37:15 -0400

    Testing

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const log = bunyan.createLogger({ name: 'tfo_slack' })
 const express = require('express')
 const app = express()
 
+const bp = require('body-parser')
+app.use(bp.json())
+app.use(bp.urlencoded({
+  extended: true
+}))
+
 app.post('/speakeasy', (req, res) => {
   log.info(req.body)
   log.info(req.query)
